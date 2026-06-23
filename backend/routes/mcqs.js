a4fbd40b3926d819965aa2eac810b3b9ceaa4d00
Noman-Prime <your-email@example.com>
2026-06-23 13:34:31 +0500

    It a functional website

---
+++ backend/routes/mcqs.js
@@ -0,0 +1,15 @@
+// import express from "express"
+// import { createMCQS, deletetMCQS, getAllMCQS, getMCQS, SSE_Stream, updateMCQS } from "../controller/mcqs.js"
+// const mcqsRouter = express.Router()
+
+// mcqsRouter.post("/create", createMCQS)
+// mcqsRouter.put("/update/:id", updateMCQS)
+// mcqsRouter.delete("/delete/:id", deletetMCQS)
+// mcqsRouter.get("/get/all", getAllMCQS)
+// mcqsRouter.get("/get/:id", getMCQS)
+// mcqsRouter.get("/stream", SSE_Stream)
+
+// export default mcqsRouter
+
+
+// // http:/localhost:5000/api/v1/mcqs/stream   --> SSE-live--Stream
